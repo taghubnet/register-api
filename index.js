c18bb261d8e695ce8ddb87dd4a5ff9ce7ed82f63
Asbjørn Enge <asbjorn@hanafjedle.net>
2017-06-30 09:22:28 +0200

    progress

--- index.js
+++ index.js
@@ -5,15 +5,30 @@ var args = require('minimist')(process.argv.slice(2), {
 })
 var express = require('express')
 var bodyParser = require('body-parser')
+
 var app = express()
+var nodes = []
+var joinToken = 'abc'
 
 app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send(JSON.stringify(req.body))
+  nodes.push(JSON.stringify(req.body)) 
+  res.send(JSON.stringify({
+    token: joinToken
+  }))
 })
 
 app.listen(args.port, () => {
   console.log('Listening on '+args.port)
 })
+
+// Register 
+setInterval(function() {
+  console.log(nodes)
+}, 10000)
+
+// Read token
+setInterval(function() {
+  console.log('reading token')
+}, 10000)
